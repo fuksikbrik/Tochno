bafdc306f6fb52a2ec9273aa281c5cdcf892c5dc
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-04-24 20:43:30 +0300

    fix DIma

--- script/script.js
+++ script/script.js
@@ -1,22 +1,4 @@
-const menuButton = document.querySelector('.header-menu');
-const menuBlock = document.querySelector('.header-menu-block');
-const quizCards = document.querySelectorAll('.card-quiz-answer');
+const cards = document.querySelectorAll('.card-quiz-answer');
+let lastCard = cards[0];
 
-
-
-menuButton.addEventListener('click', function (){
-    menuButton.classList.toggle('active-menu');
-    menuBlock.classList.toggle('disabled')
-    console.log('click')
-    
-});
-
-quizCards.forEach((card) => {
-    card.addEventListener("click", (event) => {
-		quizCards.forEach((removeClass) => {
-			removeClass.classList.remove('active');
-		});
-	  card.classList.add('active')
-    });
-  });
 
